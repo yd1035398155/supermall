79c8af796448c64e50e58521c633596b9987c39e
Wizard <1035398155@qq.com>
2021-06-03 20:38:45 +0800

    updata

--- src/main.js
+++ src/main.js
@@ -4,12 +4,23 @@ import router from "./router";
 import store from "./store";
 import { getHomeMultidata } from "network/home";
 import toast from "components/common/toast";
-
+import FastClick from "fastclick";
+import LazyLoad from "vue-lazyload";
 Vue.config.productionTip = false;
 // 事件总线
 Vue.prototype.$bus = new Vue();
+
 // 安装toast插件
 Vue.use(toast);
+
+// 解决移动端300ms延迟问题
+FastClick.attach(document.body);
+
+// 使用懒加载
+Vue.use(LazyLoad, {
+  loading: require("./assets/img/common/placeholder.png")
+});
+
 new Vue({
   router,
   store,
